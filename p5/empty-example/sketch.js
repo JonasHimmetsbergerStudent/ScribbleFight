641f57b3e7bed9e057c552ad9cb95124c0d30c87
RafetsederTobias <tobisa.rafetseder@gmx.at>
2021-06-29 11:55:03 +0200

    first move implementations

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,9 +1,45 @@
+//Objects
+var cube;
+var collider;
+
+//Forces
+var GRAVITY = -1;
+var JUMP = 15;
+var SPEED = 5;
+
 function setup() {
-  createCanvas(1920,1080);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1920, 1080);
+  cube = createSprite(400, 200, 50, 50);
+  collider = createSprite(400, 500, 200, 200);
 }
 
 function draw() {
-  background(255,255,255);  
+  cube.velocity.y -= GRAVITY;
+  cube.velocity.x = 0;
+  background(255, 255, 255);
+
+  if (cube.collide(collider)) {
+    cube.velocity.y = 0;
+  }
+
+  // Controls
+    //Spacebar
+  if (keyWentDown(32)) {
+    cube.velocity.y = -JUMP;
+  }
+    //A
+  if(keyIsDown(65)) {
+      cube.velocity.x = -SPEED;
+  }
+    //D
+  if(keyIsDown(68)) {
+      cube.velocity.x = SPEED;
+  }
+    //S
+  if(keyIsDown(83)) {
+    cube.velocity.y -= GRAVITY;
+  }
   drawSprites();
 }
+
+ 
