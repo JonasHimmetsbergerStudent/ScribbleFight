0168a8c19f30ad7556bb85386688354690344a23
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-07-13 13:27:06 +0200

    webpage of document scanner

--- prototypes/stream/app.py
+++ prototypes/stream/app.py
@@ -1,35 +1,37 @@
 from flask import Flask, render_template, Response
 import cv2
+import ssl
+context = ssl.SSLContext()
+context.load_cert_chain('fullchain.pem', 'privkey.pem')
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(1)
+# camera = cv2.VideoCapture(1)
 
 
-def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+# def gen_frames():  # generate frame by frame from camera
+#     while True:
+#         # Capture frame-by-frame
+#         success, frame = camera.read()  # read the camera frame
+#         if not success:
+#             break
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
-@app.route('/video_feed')
-def video_feed():
-    # Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     # Video streaming route. Put this in the src attribute of an img tag
+#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host="192.168.0.21")
+    app.run(debug=True, host="0.0.0.0", ssl_context=context)
