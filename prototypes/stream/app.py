2a6fab42693d9a42d879d3b530748ce2477aa710
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-07-18 14:37:11 +0200

    back to normal

--- prototypes/stream/app.py
+++ prototypes/stream/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-import cv2
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -9,5 +11,32 @@ def index():
     return render_template('index.html')
 
 
+@socketio.on('my event')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+
+@socketio.on('my image')
+def test_message(message):
+    emit('imageConversionByClient', {'buffer': message['data']})
+
+
+@socketio.on('my broadcast event')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.0.21", ssl_context='adhoc')
+    print('server running')
+    socketio.run(app, debug=True, host="192.168.0.21")
+    print('server closed')
