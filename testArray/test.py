3ce7c543d81c23e57592d4a39b97ae8494b86474
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-09-20 11:10:22 +0200

    i was ned wos i geändert hob

---
+++ testArray/test.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import numpy as np
+import ast
+
+f = open("./testArray/test.txt", "r")
+iar = ast.literal_eval(f.read())
+
+rows = len(iar)
+columns = len(iar[0])
+
+newImg = []
+# newImg = iar
+x = 0
+y = 0
+
+while y < rows:
+    newImg.append([])
+    while x < columns:
+        if iar[y][x] == [0]:
+            newImg[y].append([255, 255, 255, 255])
+        if iar[y][x] == [1]:
+            newImg[y].append([255, 0, 0, 255])
+        if iar[y][x] == [2]:
+            newImg[y].append([255, 255, 0, 255])
+
+        x += 1
+    x = 0
+    y += 1
+
+# print(newImg)
+
+pippoRGBA2 = Image.fromarray(np.array(newImg).astype('uint8'), mode='RGBA')
+pippoRGBA2.show()
