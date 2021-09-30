c32002fb83ab2ce9fd1d231d5c9f4297b001416b
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-09-30 08:12:15 +0200

    get status with cookies

--- source/AI/KI_versions/KI_v01/env/options/observations.py
+++ source/AI/KI_versions/KI_v01/env/options/observations.py
@@ -8,6 +8,22 @@ def getStats(driver):
     return dmgDealt, knockback, deaths, kills
 
 
+'''def getStats(driver):
+    cookies = driver.get_cookies()
+    dmgDealt, knockback, deaths, kills = 0, 0, 0, 0
+    for i in range(4):
+        cookie = cookies[i]
+        if cookie['name'] == u'dmgDealt':
+            dmgDealt = float(cookie['value'])
+        if cookie['name'] == u'knockback':
+            knockback = int(cookie['value'])
+        if cookie['name'] == u'death':
+            deaths = int(cookie['value'])
+        if cookie['name'] == u'kills':
+            kills = int(cookie['value'])
+    return dmgDealt, knockback, deaths, kills'''
+
+
 def getVisual(driver):
     visualArr = driver.execute_script('return visCopy')
     return visualArr
--- source/AI/KI_versions/main.py
+++ source/AI/KI_versions/main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     env = gym.make("ScribbleFight-v0")
     time.sleep(1)
 
-    episodes = 1
+    episodes = 3
     for episode in range(1, episodes+1):
         state = env.reset()
         done = False
@@ -32,6 +32,7 @@ if __name__ == "__main__":
             action = env.action_space.sample()
             n_state, reward, done, info = env.step(action)
             score += reward
+            print(score)
         print('Episode:{} Score:{}'.format(episode, score))
     env.close()
 
