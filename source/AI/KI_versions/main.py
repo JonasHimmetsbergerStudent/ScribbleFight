ca450cc3572497475c7dee709d2b0730afe286e2
j.himmetsberger@students.htl-leonding.ac.at <j.himmetsberger2@gmail.com>
2021-10-02 11:01:22 +0200

    changes done

--- source/AI/KI_versions/main.py
+++ source/AI/KI_versions/main.py
@@ -29,8 +29,8 @@ if __name__ == "__main__":
         while not done:
             env.render()
             actions = env.action_space.sample()
-            # actions[0] = 1
-            # actions[1] = 1
+            actions[0] = 1
+            actions[1] = 1
             n_state, reward, done, info = env.step(actions)
             score += reward
             print(actions)
